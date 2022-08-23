fa406cf622618ae2119cd84814a399015b061a10
Richard Walsh <richwalsh2@googlemail.com>
2022-08-23 21:21:58 +0100

    add icons to footer

--- mooch_fe_dev/src/components/Footer/Footer.js
+++ mooch_fe_dev/src/components/Footer/Footer.js
@@ -4,10 +4,12 @@ import classes from './Footer.module.css';
 const Footer = () => (
   <footer className={classes.footer_container}>
     <div>
-      <p className={classes.footer_logo}>MoOCH</p>
+      <p className={classes.footer_logo} data-heading="logo-small">
+        MoOCH
+      </p>
       <p>&copy;2022 MoOCH</p>
     </div>
-    <div>
+    <div className={classes.footer_content}>
       <h5 className={classes.footer_heading}>menu</h5>
       <ul>
         <li>Lorem</li>
@@ -22,17 +24,42 @@ const Footer = () => (
         <li>Lorem</li>
       </ul>
     </div>
-    <div>
+    <div className={classes.footer_content}>
       <h5 className={classes.footer_heading}>follow</h5>
-      <ul>
-        <li>facebook</li>
-        <li>instagram</li>
-        <li>twitter</li>
-        <li>youtube</li>
-        <li>linkedin</li>
+      <ul className={classes.footer_list__social}>
+        <li>
+          <span class="material-icons" data-font="icon">
+            facebook
+          </span>
+          facebook
+        </li>
+        <li>
+          <span class="material-icons" data-font="icon">
+            pix
+          </span>
+          instagram
+        </li>
+        <li>
+          <span class="material-icons" data-font="icon">
+            flutter_dash
+          </span>
+          twitter
+        </li>
+        <li>
+          <span class="material-icons" data-font="icon">
+            youtube_searched_for
+          </span>
+          youtube
+        </li>
+        <li>
+          <span class="material-icons" data-font="icon">
+            link
+          </span>
+          linkedin
+        </li>
       </ul>
     </div>
-    <div>
+    <div className={classes.footer_content}>
       <h5 className={classes.footer_heading}>get started</h5>
       <ul>
         <li>sign up</li>
