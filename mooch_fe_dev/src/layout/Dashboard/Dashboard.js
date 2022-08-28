e37a14310f1b6cdd7615155cf0d9683fb20c6088
Richard Walsh <richwalsh2@googlemail.com>
2022-08-28 16:50:55 +0100

    add user stats card to Dashboard.js

--- mooch_fe_dev/src/layout/Dashboard/Dashboard.js
+++ mooch_fe_dev/src/layout/Dashboard/Dashboard.js
@@ -2,11 +2,13 @@ import React from 'react';
 import classes from './Dashboard.module.css';
 
 import UserInfo from './UserInfo/UserInfo';
+import UserStats from './UserStats/UserStats';
 
 const Dashboard = () => (
   <main className={classes.dashboard}>
     <section className={classes.dashboard_user}>
       <UserInfo />
+      <UserStats />
     </section>
     <section className={classes.dashboard_recent}></section>
     <section className={classes.dashboard_segments}></section>
---
+++ mooch_fe_dev/src/layout/Dashboard/UserStats/UserStats.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import classes from './UserStats.module.css';
+
+const UserStats = () => (
+  <div className={classes.user_stats}>
+    <div className={classes.stat_btn_container}>
+      <button>
+        <span class="material-icons">assignment</span>
+      </button>
+      <button>
+        <span class="material-icons">directions_bike</span>
+      </button>
+      <button>
+        <span class="material-icons">run_circle</span>
+      </button>
+      <button>
+        <span class="material-icons">rowing</span>
+      </button>
+    </div>
+    <div>
+      <p className={classes.stat_title}>this week</p>
+      <p className={classes.stat_total}>49km</p>
+    </div>
+    <div>
+      <p className={classes.stat_title}>this year</p>
+      <p className={classes.stat_total}>249km</p>
+    </div>
+  </div>
+);
+
+export default UserStats;
