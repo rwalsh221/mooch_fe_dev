080e08f0ff9a7070f04bf20f6dc51eba2d8267c7
Richard Walsh <richwalsh2@googlemail.com>
2022-09-02 19:37:34 +0100

    stlye dashboard.js

--- mooch_fe_dev/src/layout/Dashboard/Dashboard.js
+++ mooch_fe_dev/src/layout/Dashboard/Dashboard.js
@@ -7,21 +7,23 @@ import UserStats from './UserStats/UserStats';
 import ActivityCard from './ActivityCard/ActivityCard';
 
 const Dashboard = () => (
-  <main className={classes.dashboard}>
-    <section className={classes.dashboard_user}>
-      <UserInfo />
-      <UserStats />
-    </section>
-    <section className={classes.dashboard_recent}>
-      <ActivityCard />
-      <ActivityCard />
-    </section>
-    <section className={classes.dashboard_segments}>
-      <SegmentSnapshotSmall />
-      <SegmentSnapshotSmall />
-      <SegmentSnapshotSmall />
-    </section>
-  </main>
+  <div data-wrapper="max-content-width">
+    <main className={classes.dashboard}>
+      <section className={classes.dashboard_user}>
+        <UserInfo />
+        <UserStats />
+      </section>
+      <section className={classes.dashboard_recent}>
+        <ActivityCard />
+        <ActivityCard />
+      </section>
+      <section className={classes.dashboard_segments}>
+        <SegmentSnapshotSmall />
+        <SegmentSnapshotSmall />
+        <SegmentSnapshotSmall />
+      </section>
+    </main>
+  </div>
 );
 
 export default Dashboard;
