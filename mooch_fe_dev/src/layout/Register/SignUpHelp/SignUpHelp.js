e49397e9b376e8f67d6dc0ee869bd5ebf3ffd245
Richard Walsh <richwalsh2@googlemail.com>
2023-10-05 14:04:19 +0100

    edit sign up guide

--- mooch_fe_dev/src/layout/Register/SignUpHelp/SignUpHelp.js
+++ mooch_fe_dev/src/layout/Register/SignUpHelp/SignUpHelp.js
@@ -16,35 +16,30 @@ const SignUpHelp = ({ needHelpHandlerProps }) => (
 
     <ol>
       <li>
-        1, Click on the padlock icon at the top of the page next to the URL and
-        click site settings
-      </li>
-      <li>2, Under PERMISSIONS allow INSECURE CONTENT</li>
-      <li>
-        3, Log in to strava, go to settings under you profile picture at the top
+        1, Log in to strava, go to settings under you profile picture at the top
         of the page
       </li>
       <li>
-        4, Add api to the end of the strava url:
+        2, Add api to the end of the strava url:
         https://www.strava.com/settings/api
       </li>
-      <li>5, Input anything you want for Application Name</li>
+      <li>3, Input anything you want for Application Name</li>
       <li>
-        6, Input{' '}
+        4, Input&nbsp;
         <span className={classes.alert}>https://www.localhost.com</span>
         &nbsp;for website
       </li>
       <li>
-        7, Input <span className={classes.alert}>mooch-bfeeb.web.app</span>
+        5, Input <span className={classes.alert}>mooch-bfeeb.web.app</span>
         &nbsp;for Authorization Input Domain
       </li>
-      <li>8, Complete the sign up form with your strava api details</li>
+      <li>6, Complete the sign up form with your strava api details</li>
       <li>
-        9, You dont need to use real email: something like&nbsp;
+        7, You dont need to use real email: something like&nbsp;
         <span className={classes.alert}>YOURname@email.com</span>
       </li>
       <li>
-        10, Use a simple password but use letters and numbers: something
+        8, Use a simple password but use letters and numbers: something
         like&nbsp;<span className={classes.alert}>password1</span>
       </li>
     </ol>
